0c9f9bc47228e6511d83a047975983f543b6b30f
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-30 14:21:56 -0500

    GET array of all the post objects completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+const Post = require('./posts-model');
+
+router.get('/', (req, res) => {
+  Post.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+        error: err.message,
+      });
+    });
+});
+
+module.exports = router;
