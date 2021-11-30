7e552b8a0bb1374149ee64da36b56aa0ff9e9132
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-30 15:53:29 -0500

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('./posts-model');
 
+// [GET] /api/posts	(Returns an array of all the post objects contained in the database)
 router.get('/', (req, res) => {
   Post.find(req.query)
     .then((posts) => {
@@ -16,6 +17,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// [GET] /api/posts/:id	(Returns the post object with the specified id)
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
     .then((post) => {
@@ -35,6 +37,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// [POST] /api/posts (Creates a post using the information sent inside the request body and returns the newly created post object)
 router.post('/', (req, res) => {
   if (!req.body.title || !req.body.contents) {
     res.status(400).json({
@@ -54,6 +57,7 @@ router.post('/', (req, res) => {
   }
 });
 
+// [PUT] /api/posts/:id	(Updates the post with the specified id using data from the request body and returns the modified document, not the original)
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
@@ -87,6 +91,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// [DELETE] /api/posts/:id	(Removes the post with the specified id and returns the deleted post object)
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
   Post.findById(id)
@@ -117,6 +122,26 @@ router.delete('/:id', (req, res) => {
         error: err.message,
       });
     });
+});
+
+// [GET] /api/posts/:id/comments (Returns an array of all the comment objects associated with the post with the specified id)
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comments = await Post.findPostComments(id);
+    if (comments.length) {
+      res.status(200).json(comments);
+    } else {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The comments information could not be retrieved',
+      error: err.message,
+    });
+  }
 });
 
 module.exports = router;
